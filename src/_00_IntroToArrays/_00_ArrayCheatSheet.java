af4b10ac14a9b4057d62b7e45b1bcf4518744c31
gabrielsong9674 <gracegabrielson@icloud.com>
2019-09-01 17:30:34 -0700

    RobotRace(IP)

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -4,30 +4,52 @@ import java.util.Random;
 
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
+		// 1. make an array of 5 Strings
+		String[] string = { "one", "two", "three", "four", "five" };
+		// 2. print the third element in the array
+		System.out.println(string[2]);
+		// 3. set the third element to a different value
+		string[2] = "six";
+		// 4. print the third element again
+		System.out.println(string[2]);
+		// 5. use a for loop to set all the elements in the array to a string of your
+		// choice
+		for (int i = 0; i < string.length; i++) {
+			string[i] = "Grace";
+		}
+		// 6. use a for loop to print all the values in the array
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
+		for (int i = 0; i < string.length; i++) {
+			System.out.println(string[i]);
+		}
+		// 7. make an array of 50 integers
+		int intArray[] = new int[50];
+		// 8. use a for loop to make every value of the integer array a random number
+		Random num = new Random();
+		int smallestNum = 50;
+		int largestNum = 0;
+		int lastNum = 0;
+		for (int i = 0; i < intArray.length; i++) {
+			intArray[i] = num.nextInt(50);
+			if (intArray[i] < smallestNum) {
+				smallestNum = intArray[i];
+			}
 
-		//2. print the third element in the array
+			if (intArray[i] > largestNum) {
+				largestNum = intArray[i];
+			}
+			lastNum = intArray[49];
+			// 10 print the entire array to see if step 8 was correct
+			System.out.println(intArray[i]);
+		}
+		// 9. without printing the entire array, print only the smallest number on the
+		// array
+		System.out.println("Smallest number: " + smallestNum);
+		// 11. print the largest number in the array.
+		System.out.println("Largest number: " + largestNum);
 
-		//3. set the third element to a different value
+		// 12. print only the last element in the array
+		System.out.println("Last number: " + lastNum);
 
-		//4. print the third element again
-		
-		//5. use a for loop to set all the elements in the array to a string of your choice
-		
-		//6. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		//7. make an array of 50 integers
-
-		//8. use a for loop to make every value of the integer array a random number
-
-		//9. without printing the entire array, print only the smallest number on the array
-
-		//10 print the entire array to see if step 8 was correct
-
-		//11. print the largest number in the array.
-		
-		//12. print only the last element in the array
-		
 	}
 }
--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -7,24 +7,30 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
-	//1. make a main method
-	
+	// 1. make a main method
+	public static void main(String[] args) {
 		//2. create an array of 5 robots.
-
+		Robot[] robotArray = new Robot[5];
 		//3. use a for loop to initialize the robots.
-
-			//4. make each robot start at the bottom of the screen, side by side, facing up
-	
+		for (int i = 0; i < robotArray.length; i++) {
+			 robotArray[i] = new Robot();
+		//4. make each robot start at the bottom of the screen, side by side, facing up
+			 robotArray[i].setY(500);
+			 robotArray[i].setX(180*i+50);
+		}		
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
-    	
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
+    		for (int i = 0; i < robotArray.length; i++) {
+    			Random r = new Random();
+			robotArray[i].move(r.nextInt(50));
+    		}
+    	
 		//7. declare that robot the winner and throw it a party!
     	
 		//8. try different races with different amounts of robots.
     	
 	    //9. make the robots race around a circular track.
 
-	
+	}
 }
